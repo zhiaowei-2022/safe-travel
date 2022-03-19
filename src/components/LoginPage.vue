c59bdc0d509e2939a82ef564b003fee8d1c58a81
josephleejj <josephleejj@hotmail.sg>
2022-03-19 15:47:48 +0800

    layout for login done

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -42,7 +42,7 @@
         </tr>
 
     </table>
-    
+
     <button @click="signOut" v-if="isLoggedIn"> Sign out</button>
     <p v-if="isLoggedIn" id="displayUsername"> </p>
 
@@ -53,12 +53,126 @@
 </template>
 
 <script>
+import firebaseApp from '../firebase.js';
+import {getFirestore} from "firebase/firestore"
+import { doc, getDoc } from "firebase/firestore";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import { ref } from "vue";
+
+const db = getFirestore(firebaseApp);
+
 export default {
-    name: "LoginPage"
+    name: 'LoginPage',
+
+  data() {
+    return {
+      showPassword: false,
+      isLoggedIn: ref(false),
+      auth: getAuth(),
+      username: "",
+    };
+  },
+  methods: {
+    toggleShow() {
+      if (this.showPassword == true) {
+        this.showPassword = false
+      } else{
+        this.showPassword = true
+      }
+    },
+    LogIn() {
+      var email = document.getElementById("email").value
+      var password = document.getElementById("password").value
+      const auth = getAuth();
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user.email)
+          console.log("signed in")
+        })
+        .catch((error) => {
+          console.log(error.code)
+          switch (error.code) {
+            case "auth/invalid-email":
+              alert("Invalid Email");
+              break;
+            default:
+              alert("Incorrect Email/Password")
+              break;
+          }
+        });
+
+    },
+    signOut() {
+      signOut(this.auth).then(() => {
+        console.log("signed out")
+      })
+    },
+
+  },
+    beforeMount() {
+    onAuthStateChanged(this.auth, (user) => {
+      if (user) {
+        this.isLoggedIn = true;
+        display(user.email)
+
+      } else {
+        this.isLoggedIn = false;
+      }
+    })
+      async function display(email) {
+        const docRef = doc(db, "Users", email);
+          const docSnap = await getDoc(docRef);
+
+          if (docSnap.exists()) {
+            console.log("Document data:", docSnap.data());
+            document.getElementById("displayUsername").innerHTML = docSnap.data().username
+          } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+          }
+      }
+    }
 
 }
 </script>
 
-<style>
+<style scoped>
+    .signInForm {
+            max-width: 190px;
+            width: 100%;
+            background-color: #f0f0f0;
+            height: 30px;
+            border-radius: 55px;
+            border: 3px solid transparent;
+            padding: 0 0.4rem;
+            font-size: 15px;
+            margin: 20px auto;
+            
+    }
+
+
+    #register {
+        font-size: 10px;
+    }
+
+    table{
+      margin-left:auto;
+      margin-right:auto;
+      text-align: right;
+
+    }
+    td{
+      padding-left:10px;
+    }
+
+    #loginBtn {
+      background-color: rgb(0, 15, 92);
+      color: white;
+      font-weight: bold;
+      width: 120px;
+      height: 50px;
+      align-items: center;
+    }
 
 </style>
