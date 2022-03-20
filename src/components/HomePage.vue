2312f881b526290564000c9577c46d7974123618
josephleejj <josephleejj@hotmail.sg>
2022-03-20 17:27:54 +0800

    skeleton for homepage's search done

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,15 +3,35 @@
     <link rel= "stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
-<div v-if="user">
-  <button id = "btn" @click="signOut()">Logout</button> <br>
-    {{ user.displayName }}
-  </div>
-  <div v-else>
-      <p> unknown user</p>
-  </div>
 
-
+    <!-- Form for Book Flight Search Fields -->
+    <div class="book-flight">
+        <h1>Guidelines</h1>
+        <form class=form-details>
+        <div class="row">
+          <!-- Search field for origin country -->
+          <div class="col">
+           <label for="origin-country" class="title">Origin</label>
+           <input type="text" class="form-control" id="origin-country-input" placeholder="Enter origin country" v-model="originCountry" required>
+          </div>
+          <!-- Search field for destination country -->
+          <div class="col">
+            <label for="destination-country" class="title">Destination</label>
+            <input type="text" class="form-control" id="destination-country-input" placeholder="Enter destination country" v-model="destinationCountry" required>
+           </div>
+        </div>
+        <br>
+        <div class="row g-lg-2">
+            <div class="form-group">
+                <div>
+                 <button class="btn btn-primary " name="submit" type="submit" @click="searchInfo()">
+                  Display Information
+                 </button>
+                </div>
+            </div>
+        </div>
+        </form>
+    </div>
 
 
 
@@ -30,7 +50,7 @@ import { getAuth, onAuthStateChanged, signOut } from "firebase/auth"
 
 export default {
     name: "HomePage",
-
+    
     data() {
         return {
             user: false,
@@ -63,5 +83,48 @@ export default {
 </script>
 
 <style>
+    .book-flight {
+        background-image: url("@/assets/holding-earth.jpg");
+        height: 50vh;
+        /* padding-top: 100px; */
+        background-size: cover;
+              background-position: center;
+      background-repeat: no-repeat;
+    }
+    h1 {
+    text-align: left;
+    margin-left: 180px;
+    font-weight: bold;
+    color: rgb(1, 1, 87);
+    }
+    h2 {
+    text-align: left;
+    margin-left: 80px;
+    font-weight: bold;
+    color: rgb(1, 1, 87);
+    }   
+    button {
+        background-color: lightskyblue;
+        border-color: lightskyblue;
+        color: black;
+        font-weight: bold;
+    }
+    img {
+        width: 100%;
+        height: 100%;
+        border-radius: 10px;
+    }
+    .form-details {
+        background-color: rgb(1, 1, 87);
+        padding: 20px;
+        border-radius: 10px;
+        margin-right: 180px;
+        margin-left: 180px;
+    }
+    label {
+        color: white;
+        float: left;
+        text-align: left;
+    }
 
 </style>
--- src/components/TryingStuffs.vue
+++ src/components/TryingStuffs.vue
@@ -2,16 +2,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel= "stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-
+    
 <div v-if="user">
   <button id = "btn" @click="signOut()">Logout</button> <br>
-  <div v-if="anonymous">
-    <p> unknown </p>
-    {{ user }}
+    {{ user.displayName }}
   </div>
   <div v-else>
-  {{ user.displayName }}
-  </div>
+      <p> unknown user</p>
   </div>
 
   <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
@@ -77,7 +74,6 @@ export default {
     data() {
         return {
             user: false,
-            anonymous: false,
             invalid: false,
         }
     },
