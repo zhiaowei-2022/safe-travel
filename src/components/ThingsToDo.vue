e45d5a77839091d15a8904cdcbf4bb053766b07c
brandonyhr98 <brandonyhr@gmail.com>
2022-03-24 19:39:23 +0800

    Update ThingsToDo.vue
    created a modal . but have not push info into the modal

--- src/components/ThingsToDo.vue
+++ src/components/ThingsToDo.vue
@@ -1,7 +1,24 @@
 <template>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
 
+    <div class="ThingsToDo">
+        <h1>Things To Do</h1>
+        <h3>Explore attractions, tours and more</h3>
+        <form class="form-details">
+        <div class="row">
+            <div class="col-lg-2">
+            <select name="country" class="form-select form-control">
+                <option value="sgp" selected>Singapore</option>
+                <option value="jpn">Japan</option>
+                <option value="tha">Thailand</option>
+            </select>
+            </div>
+        </div>
+        </form>
+    </div>
+    <br>
     <div class="container">
         <div class="row">
             <div class="col" id="overview"><button v-on:click='display("overview")'>Overview</button></div>
@@ -13,7 +30,7 @@
     </div>
     <br><br>
     <div class="container">
-        <h4 style='text-align:left'><b>Popular Attractions</b></h4>
+        <h4><b>Popular Attractions</b></h4>
         <div class="searchResult">
             <div class="row">
                 <div class="col" id="0">
@@ -33,7 +50,25 @@
             </div>
         </div>
     </div>
+    <div id="searchResult" class="modal">
 
+    <!-- Modal content -->
+    <div class="modal-content">
+        <span class="close" v-on:click="closeModal()">&times;</span>
+        <p>Some text in the Modal..</p>
+        <!-- 
+            img
+            Name
+            Rating
+            Address
+            Phone
+
+            Description
+
+            Website
+        -->
+    </div>
+    </div>
 </template>
 
 <script>
@@ -54,6 +89,12 @@ export default {
             
             
         },
+
+        closeModal() {
+            var modal = document.getElementById("searchResult");
+            modal.style.display = "none";
+        },
+
         async display(variable){
             let z = await getDocs(collection(db, "ThingToDo"))
             
@@ -74,24 +115,37 @@ export default {
                 }
                 else {
                     if (variable === undefined || variable == 'overview') {
+
+    
                         
-                        container.innerHTML +=  "<figure>" 
+                        container.innerHTML +=  "<figure id='"+name+"'>" 
                         + "<img src='"+ imageURL +"'style='width:100%'>"
                         + "<figcaption>" + name + " </figcaption>"
                         + "</figure>"  
+
+                        
+
                         ind+=1
                     }
                     else {
                         var category = (yy.Category)
                         if (variable == category) {
-                            container.innerHTML +=  "<figure>" 
+                            container.innerHTML +=  "<figure id='"+name+"'>" 
                             + "<img src='"+ imageURL +" 'style='width:100%'>"
                             + "<figcaption>" + name + " </figcaption>"
                             + "</figure>" 
                             ind+=1
                         }
                     }
-                    
+                    container.onclick = function() {
+                            var modal = document.getElementById("searchResult");
+                            console.log("getting modal");
+
+                            // need to insert Information into Modal
+
+                            modal.style.display = "block";
+                            console.log("modal displayed")
+                    }
                 }
             })
             
@@ -104,6 +158,23 @@ export default {
 </script>
 
 <style scoped>
+.ThingsToDo {
+  background-image: url("https://res.klook.com/image/upload/fl_lossy.progressive,q_85/c_fill,,w_1920,/v1607408071/ued/ttd/banner/jpg/ttd_veritcal_page_banner_experiences.jpg"); 
+  height: 400px;
+  padding-top: 100px;
+}
+h1, h3 {
+  text-align: left;
+  margin-left: 180px;
+  font-weight: bold;
+  color: white;
+}
+h4 {
+    text-align: left;
+    margin-left: 30px;
+    font-weight: bold;
+    color: rgb(1, 1, 87);
+}
 .row {
     background-color: rgb(0, 15, 95, 0.05);
     align-items: center;
@@ -119,4 +190,56 @@ button {
     background-color: rgb(0, 15, 95, 0.05);
     color: rgb(0, 15, 95);
 }
+.form-details {
+  padding: 20px;
+  border-radius: 10px;
+  margin-right: 180px;
+  margin-left: 160px;
+}
+label {
+  color: white;
+  float: left;
+  text-align: left;
+}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  /* overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 50%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button */
+.close {
+  color: #aaa;
+  text-align: right;
+  padding-right:15px;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+
 </style>
