ec75f4f4f96539c8388d4a978692e77639cfcf50
brandonyhr98 <brandonyhr@gmail.com>
2022-03-25 17:10:22 +0800

    Update ThingsToDo.vue

--- src/components/ThingsToDo.vue
+++ src/components/ThingsToDo.vue
@@ -1,70 +1,61 @@
 <template>
-  <!-- Bootstrap CSS -->
-  <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-    crossorigin="anonymous"/>
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
 
-  <div class="ThingsToDo">
-    <h1>Things To Do</h1>
-    <h3>Explore attractions, tours and more</h3>
-    <form class="form-details">
-      <div class="row">
-        <div class="col-lg-2">
-          <select name="country" class="form-select form-control">
-            <option value="sgp" selected>Singapore</option>
-            <option value="jpn">Japan</option>
-            <option value="tha">Thailand</option>
-          </select>
+    <div class="ThingsToDo">
+        <h1>Things To Do</h1>
+        <h3>Explore attractions, tours and more</h3>
+        <form class="form-details">
+        <div class="row">
+            <div class="col-lg-2">
+            <select name="country" class="form-select form-control">
+                <option value="sgp" selected>Singapore</option>
+                <option value="jpn">Japan</option>
+                <option value="tha">Thailand</option>
+            </select>
+            </div>
         </div>
-      </div>
-    </form>
-  </div>
-  <br />
-  <div class="container">
-    <div class="row">
-      <div class="col" id="overview">
-        <button v-on:click="display('overview')">Overview</button>
-      </div>
-      <div class="col" id="Museum">
-        <button v-on:click="display('Museum')">Museum & Galleries</button>
-      </div>
-      <div class="col" id="WTP">
-        <button v-on:click="display('WTP')">Water & Themed Park</button>
-      </div>
-      <div class="col" id="Thrill">
-        <button v-on:click="display('Thrill')">Thrill Activities</button>
-      </div>
-      <div class="col" id="AquaZoo">
-        <button v-on:click="display('AquaZoo')">Aquarium & Zoos</button>
-      </div>
+        </form>
     </div>
-  </div>
-  <br /><br />
-  <div class="container">
-    <h4><b>Popular Attractions</b></h4>
-    <div class="searchResult">
-      <div class="row">
-        <div class="col" id="0"></div>
-        <div class="col" id="1"></div>
-        <div class="col" id="2"></div>
-      </div>
-      <div class="row">
-        <div class="col" id="3"></div>
-        <div class="col" id="4"></div>
-        <div class="col" id="5"></div>
-      </div>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col" id="overview"><button v-on:click='display("overview")'>Overview</button></div>
+            <div class="col" id="Museum"><button v-on:click='display("Museum")'>Museum & Galleries</button></div>
+            <div class="col" id="WTP"><button v-on:click='display("WTP")'>Water & Themed Park</button></div>
+            <div class="col" id="Thrill"><button v-on:click='display("Thrill")'>Thrill Activities</button></div>
+            <div class="col" id="AquaZoo"><button v-on:click='display("AquaZoo")'>Aquarium & Zoos</button></div>
+        </div>
     </div>
-  </div>
-  <div id="searchResult" class="modal">
+    <br><br>
+    <div class="container">
+        <h4><b>Popular Attractions</b></h4>
+        <div class="searchResult">
+            <div class="row">
+                <div class="col" id="0">
+                </div>
+                <div class="col" id="1">
+                </div>
+                <div class="col" id="2">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col" id="3">
+                </div>
+                <div class="col" id="4">
+                </div>
+                <div class="col" id="5">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="searchResult" class="modal">
+
     <!-- Modal content -->
     <div class="modal-content">
-      <span class="close" v-on:click="closeModal()">&times;</span>
-      <div class="container">
+        <span class="close" v-on:click="closeModal()">&times;</span>
+        <div class="container">
         <div class="row">
             <div id="photo">
             <!-- img -->
@@ -77,17 +68,19 @@
             </div>            
         </div>
         </div>
-      </div>
+        
+            
     </div>
-  </div>
+    </div>
 </template>
 
 <script>
-import firebaseApp from "../firebase.js";
-import { getAuth, onAuthStateChanged  } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
-import { collection, getDocs } from "firebase/firestore";
+import firebaseApp from "../firebase.js";
+//import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
+
+import { collection, getDocs } from "firebase/firestore"
 const db = getFirestore(firebaseApp);
 export default {
     methods: {
@@ -125,121 +118,91 @@ export default {
                 else {
                     if (variable === undefined || variable == 'overview') {
 
-        if (ind > 5) {
-          return;
-        } else {
-          if (variable === undefined || variable == "overview") {
-            container.innerHTML +=
-              "<figure id='" +
-              name +
-              "'>" +
-              "<img src='" +
-              imageURL +
-              "'style='width:100%'>" +
-              "<figcaption>" +
-              name +
-              " </figcaption>" +
-              "</figure>";
+    
+                        
+                        container.innerHTML +=  "<figure id='"+name+"'>" 
+                        + "<img src='"+ imageURL +"'style='width:100%'>"
+                        + "<figcaption>" + name + " </figcaption>"
+                        + "</figure>"  
 
-            ind += 1;
-          } else {
-            var category = yy.Category;
-            if (variable == category) {
-              container.innerHTML +=
-                "<figure id='" +
-                name +
-                "'>" +
-                "<img src='" +
-                imageURL +
-                " 'style='width:100%'>" +
-                "<figcaption>" +
-                name +
-                " </figcaption>" +
-                "</figure>";
-              ind += 1;
-            }
-          }
-          container.onclick = function () {
-            var modal = document.getElementById("searchResult");
-            console.log(modal);
+                        
 
-            // need to insert Information into Modal
-            var photoinfo = document.getElementById("photo");
-            photoinfo.innerHTML =
-              "<img src='" + imageURL + " 'style='width:100%'>";
+                        ind+=1
+                    }
+                    else {
+                        var category = (yy.Category)
+                        if (variable == category) {
+                            container.innerHTML +=  "<figure id='"+name+"'>" 
+                            + "<img src='"+ imageURL +" 'style='width:100%'>"
+                            + "<figcaption>" + name + " </figcaption>"
+                            + "</figure>" 
+                            ind+=1
+                        }
+                    }
+                    container.onclick = function() {
+                            var modal = document.getElementById("searchResult");
+                            console.log("getting modal");
 
-            var resultbox = document.getElementById("resultinfo");
-            resultbox.innerHTML =
-              "<h4><b>" +
-              name +
-              "</b></h4>" +
-              "<b>Rating:</b> " +
-              rating +
-              " / 5 <br>" +
-              "<b>Address:</b> " +
-              address +
-              "<br>" +
-              "<b>Phone:</b> " +
-              contact +
-              "<br><br>" +
-              "<h5><b>Description:</b></h5> " +
-              desc +
-              "<br><br>" +
-              "For more information please visit <a href='" +
-              web +
-              "' target='_blank' style='color:black'>here</a> <br>";
-            modal.style.display = "block";
-            console.log("modal displayed");
-          };
+                            // need to insert Information into Modal
+                            var photoinfo = document.getElementById("photo")
+                            photoinfo.innerHTML = 
+                                "<img src='"+ imageURL +" 'style='width:100%'>" 
+                                
+                            var resultbox = document.getElementById("resultinfo")
+                            resultbox.innerHTML =
+                                "<h4><b>" + name + "</b></h4>" +
+                                "<b>Rating:</b> " + rating + " / 5 <br>" +
+                                "<b>Address:</b> " + address + "<br>" +
+                                "<b>Phone:</b> " + contact + "<br><br>" + 
+                                
+                                "<h5><b>Description:</b></h5> " + desc + "<br><br>" +
+                                "For more information please visit <a href='" + web + "' target='_blank' style='color:black'>here</a> <br>" 
+                            modal.style.display = "block";
+                            console.log("modal displayed")
+                    }
+                }
+            })
+            
         }
-      });
     },
-  },
-  mounted() {
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-          this.user = user;
-      }    
-    });
-    this.display();
-  },
-};
+    mounted(){
+        this.display()
+    }
+}
 </script>
 
 <style scoped>
 .ThingsToDo {
-  background-image: url("https://res.klook.com/image/upload/fl_lossy.progressive,q_85/c_fill,,w_1920,/v1607408071/ued/ttd/banner/jpg/ttd_veritcal_page_banner_experiences.jpg");
+  background-image: url("https://res.klook.com/image/upload/fl_lossy.progressive,q_85/c_fill,,w_1920,/v1607408071/ued/ttd/banner/jpg/ttd_veritcal_page_banner_experiences.jpg"); 
   height: 400px;
   padding-top: 100px;
 }
-h1,
-h3 {
+h1, h3 {
   text-align: left;
   margin-left: 180px;
   font-weight: bold;
   color: white;
 }
 h4 {
-  text-align: left;
-  margin-left: 30px;
-  font-weight: bold;
-  color: rgb(1, 1, 87);
+    text-align: left;
+    margin-left: 30px;
+    font-weight: bold;
+    color: rgb(1, 1, 87);
 }
 .row {
-  background-color: rgb(0, 15, 95, 0.05);
-  align-items: center;
-  margin: 10px 0px;
+    background-color: rgb(0, 15, 95, 0.05);
+    align-items: center;
+    margin: 10px 0px;
 }
 img {
-  width: 120px;
-  height: 120px;
-  border-radius: 50%;
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
 }
 button {
-  margin-bottom: 15px;
-  background-color: rgb(0, 15, 95, 0.05);
-  color: rgb(0, 15, 95);
+    margin-bottom: 15px;
+    background-color: rgb(0, 15, 95, 0.05);
+    color: rgb(0, 15, 95);
 }
 .form-details {
   padding: 20px;
@@ -263,8 +226,8 @@ label {
   width: 100%; /* Full width */
   height: 100%; /* Full height */
   /* overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0, 0, 0); /* Fallback color */
-  background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
 }
 
 /* Modal Content/Box */
@@ -280,7 +243,7 @@ label {
 .close {
   color: #aaa;
   text-align: right;
-  padding-right: 15px;
+  padding-right:15px;
   font-size: 28px;
   font-weight: bold;
 }
@@ -293,6 +256,9 @@ label {
 }
 
 #resultinfo {
-  text-align: left;
+    text-align: left;
 }
+
+
+
 </style>
