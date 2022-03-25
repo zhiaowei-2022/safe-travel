2f46285bae1ed52cfa20fdfeab295355af509164
josephleejj <josephleejj@hotmail.sg>
2022-03-25 18:47:58 +0800

    something wrong

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import FoodAndDining from '@/components/FoodAndDining.vue'
+import ThingsToDo from '@/components/ThingsToDo.vue'
+import AccomodationPage from '@/components/AccomodationPage.vue'
+import AccomodationResults from '@/components/AccomodationResults.vue'
+import BookFlight from '@/components/BookFlight.vue'
+import FlightResults from '@/components/FlightResults.vue'
+// import checkFB from '@/components/checkFB.vue'
 import LoginView from '@/views/LoginView.vue'
 import RegisterView from '@/views/RegisterView.vue'
 import HomeView from '@/views/HomeView.vue'
 import TryingStuffs from '@/components/TryingStuffs.vue'
 
 const routes = [
+    {
+        path: '/FoodAndDining',
+        name: 'FoodAndDining',
+        component: FoodAndDining
+    },
+    {
+        path: '/ThingsToDo',
+        name: 'ThingsToDo',
+        component: ThingsToDo
+    },
+    {
+        path: '/accomodationpage',
+        name: 'AccomodationPage',
+        component: AccomodationPage
+    },
+    {
+        path: '/accomodationresults',
+        name: 'AccomodationResults',
+        component: AccomodationResults
+    },
+    {
+        path: '/bookflight',
+        name: 'BookFlight',
+        component: BookFlight
+    },
+    {
+        path: '/flightresults',
+        name: 'FlightResults',
+        component: FlightResults
+    },    
     {
         path: '/',
         name: 'HomeView',
@@ -26,10 +62,10 @@ const routes = [
         name: 'TryingStuffs',
         component: TryingStuffs
     },
-
 ]
 const router = createRouter({
-    history: createWebHistory(), routes
+    history: createWebHistory(),
+    routes
 })
 
 router.beforeEach((to, from, next) => {
