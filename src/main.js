5a862bf8423fcbd5d0b0bc849553663acf2b9fa3
haveneshb15 <haveneshb.15@gmail.com>
2022-03-28 16:29:17 +0800

    FoodAndDining - minor changes

--- src/main.js
+++ src/main.js
@@ -1,19 +1,17 @@
-window.$ = window.jQuery = require('jquery');
+window.$ = window.jQuery = require("jquery");
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index.js'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index.js";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fas } from "@fortawesome/free-solid-svg-icons";
 library.add(fas);
 import { dom } from "@fortawesome/fontawesome-svg-core";
 dom.watch();
 
-
 const app = createApp(App).use(router);
-// const app = createApp(App);
 
 app.component("font-awesome-icon", FontAwesomeIcon);
 
